d6e1fb6bc7975ac0856e78335b8962334aa49b49
BT_1N3_30@bktech.local <BT_1N3_30@bktech.local>
2018-12-13 10:15:20 -0500

    Initial commit

---
+++ src/sample/CSVUtilities.java
@@ -0,0 +1,52 @@
+package sample;
+
+import java.awt.print.Book;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+public class CSVUtilities
+{
+    ArrayList<String> CSVData;
+    int numColumns;
+
+    public CSVUtilities(File csv)
+    {
+        String[] data;
+
+        Path pathToFile = null;
+        try (BufferedReader br = Files.newBufferedReader(pathToFile,
+                StandardCharsets.US_ASCII)) {
+
+            // read the first line from the text file
+            String line = br.readLine();
+
+            // loop until all lines are read
+            while (line != null) {
+
+                // use string.split to load a string array with the values from
+                // each line of
+                // the file, using a comma as the delimiter
+                String[] attributes = line.split(",");
+
+                Book book = createBook(attributes);
+
+                // adding book into ArrayList
+                books.add(book);
+
+                // read next line before looping
+                // if end of file reached, line would be null
+                line = br.readLine();
+            }
+
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+
+
+}
