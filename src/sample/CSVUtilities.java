63368f9793ba7fc69902e1890f9dee98ff9b5a42
Mir Shahiduzzaman <mshahiduzzaman8068@bths.edu>
2018-12-14 10:15:21 -0500

    Added other CVS and started to edit CSVUtilities Class. Need to work on this during weekend.
    Signed-off-by: Mir Shahiduzzaman <mshahiduzzaman8068@bths.edu>

--- src/sample/CSVUtilities.java
+++ src/sample/CSVUtilities.java
@@ -1,24 +1,31 @@
 package sample;
 
-import java.awt.print.Book;
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
 
 public class CSVUtilities
 {
-    ArrayList<String> CSVData;
-    int numColumns;
+    public static void main(String... args) {
+        ArrayList<String> routes = readBooksFromCSV("Routes.csv");
 
-    public CSVUtilities(File csv)
-    {
-        String[] data;
+        // let's print all the person read from CSV file
+        for (Book b : books) {
+            System.out.println(b);
+        }
+    }
 
-        Path pathToFile = null;
+    public static List<Book> readBooksFromCSV(String fileName) {
+        List<Book> books = new ArrayList<>();
+        Path pathToFile = Paths.get(fileName);
+
+        // create an instance of BufferedReader
+        // using try with resource, Java 7 feature to close resources
         try (BufferedReader br = Files.newBufferedReader(pathToFile,
                 StandardCharsets.US_ASCII)) {
 
@@ -46,7 +53,61 @@ public class CSVUtilities
         } catch (IOException ioe) {
             ioe.printStackTrace();
         }
+
+        return books;
     }
 
+    public static Book createBook(String[] metadata) {
+        String name = metadata[0];
+        int price = Integer.parseInt(metadata[1]);
+        String author = metadata[2];
+
+        // create and return book of this metadata
+        return new Book(name, price, author);
+    }
 
 }
+
+/*
+class Book {
+    private String name;
+    private int price;
+    private String author;
+
+    public Book(String name, int price, String author) {
+        this.name = name;
+        this.price = price;
+        this.author = author;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "Book [name=" + name + ", price=" + price + ", author=" + author
+                + "]";
+    }
+}
+*/
